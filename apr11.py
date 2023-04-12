34e81212aaa4f5e658c041c0c9bc4c8d8e91d2d9
alakhpandya <alakhpandya@gmail.com>
2023-04-12 18:29:03 +0530

    Create apr11.py

---
+++ apr11.py
@@ -0,0 +1,32 @@
+# taking inputs:
+"""
+print("Enter a:")
+a = input()
+# print(a)
+print("a =", a)
+
+b = input("Enter b: ")
+print("b =", b)
+
+a = int(a)
+b = float(b)
+c = a + b
+
+#  c = int(a) + float(b)
+print("sum =", c)
+
+print("type of a:", type(a))
+print("type of b:", type(b))
+"""
+# Concatenation: To concat
+"""
+p = "abc"
+q = "pqr"
+print(p + q)
+r = "13"
+print(type(r))
+"""
+
+
+a = int(input("Enter a: "))
+b = float(input("Enter b: "))
