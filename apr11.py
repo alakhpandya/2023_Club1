8f888292c53287b5801c314243cf4bc67584b6a7
alakhpandya <alakhpandya@gmail.com>
2023-04-19 14:05:05 +0530

    apr18

--- apr11.py
+++ apr11.py
@@ -30,3 +30,20 @@ print(type(r))
 
 a = int(input("Enter a: "))
 b = float(input("Enter b: "))
+
+"""
+2. Draw a flow chart for the program that controls the traffic lights of a cross road. The functions of the programs are as below:
+First, the Green light starts and is kept on until all the vehicle passes away. It takes 25s. Then the orange light starts instead of green and remains on for next 5s so that the next drivers can slow down. After the orange light turns off, the red light goes on and stays on till the next 90s so that the traffic from other three sides of the cross road can move one by one. This process keeps on repeating the entire day.
+
+3. The new mayor likes fancy things. She wants the orange light to 'blink' after every second while it is on. Change your flow chart in order to serve the purpose.
+
+4. The government noticed that there is almost no traffic after 10pm. So after 10pm, they want to keep only the orange light on till 8am. Then after, the same process as in Q-2 shall repeat. Modify your flow chart to facilitate the new requirements.
+
+"""
+
+
+
+
+
+
+
