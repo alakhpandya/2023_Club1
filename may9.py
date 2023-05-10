f63fa1e22bd3ab9f51cb1f105804f27fb4155d53
alakhpandya <alakhpandya@gmail.com>
2023-05-10 21:02:48 +0530

    Create may9.py

---
+++ may9.py
@@ -0,0 +1,41 @@
+"""
+a = int(input("a : "))
+b = int(input("b : "))
+# print("The average of a & b is:", (a + b)/2)
+
+# Output: The average of 20 & 25 is: 22.5
+
+print("The average of", a, "&", b, "is:", (a + b)/2)
+"""
+# Ask 5 integers from user and print their average on the screen.
+
+# Ask name & surname from the user & Use two print() statements to produce the output shown in the example:
+"""
+example:
+input:
+name: Royal
+surname: Technosoft
+
+Output:
+Full Name: Royal, Technosoft
+
+name=input("name: ")
+surname=input("surname: ")
+print("full name is:", name, end=", ")
+print(surname)
+
+
+print("Hello World!", end="\b")
+print("#")
+"""
+# Ask 5 integers from user and print their average on the screen as shown in the following example:
+
+# input
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+
+# Output
+The average of 1, 2, 3, 4 & 5 is: 3.0
