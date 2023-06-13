5b2f9124672d92dc3658c676c280501909ee3d48
alakhpandya <alakhpandya@gmail.com>
2023-06-13 21:03:46 +0530

    Create jun13.py

---
+++ jun13.py
@@ -0,0 +1,61 @@
+# print("Sub\tMarks".expandtabs(30))
+# print("C\t30".expandtabs(30))
+# print("Core Python\t35".expandtabs(30))
+# print("C++\t32".expandtabs(30))
+# print("Environmental Technologies\t38".expandtabs(30))
+# print("java\t28".expandtabs(30))
+# print("Computer Science\t29".expandtabs(30))
+
+
+# Write a program & take names & marks of 4 subjects from user and create a proper table of sub and marks
+"""
+print("Enter names of 4 subjects:")
+s1 = input()
+s2 = input()
+s3 = input()
+s4 = input()
+print("Enter marks of every subject:")
+m1 = int(input("Subject-1: "))
+m2 = int(input("Subject-2: "))
+m3 = int(input("Subject-3: "))
+m4 = int(input("Subject-4: "))
+
+l1 = len(s1)
+l2 = len(s2)
+l3 = len(s3)
+l4 = len(s4)
+
+max_len = max(l1, l2, l3, l4)
+print(max_len)
+tab_size = max_len + 4
+
+print("sub\tmarks".expandtabs(tab_size))
+print(f"{s1}\t{m1}".expandtabs(tab_size))
+print(f"{s2}\t{m2}".expandtabs(tab_size))
+print(f"{s3}\t{m3}".expandtabs(tab_size))
+print(f"{s4}\t{m4}".expandtabs(tab_size))
+"""
+
+# s1 = "We are glad to inform that we have upgraded our Complete Health Insurance policy and made our coverage more inclusive."
+s1 = "Congo!"
+
+# print(s1.find("C"))
+# index_of_C = s1.find("C")
+# first_o = s1.find("o")
+# print(first_o)
+
+# print(s1.find("policy"))
+# print(s1.find("in"))
+
+# print(s1.find("Z"))
+# print(s1.find("india"))
+
+# print(s1.find("in", 30, 70))
+# print(s1.find("o", 14))
+# print(s1.find("o", first_o+1))
+
+
+
+# print(s1.find("o", s1.find("o")+1))
+
+# Next Class: Classwork based on .find()
