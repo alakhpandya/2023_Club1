6c3758b419b09d9e2fd18a2ff6b7223c7fbeb42d
alakhpandya <alakhpandya@gmail.com>
2023-06-23 22:58:40 +0530

    Create jun20.py

---
+++ jun20.py
@@ -0,0 +1,6 @@
+# Write a program that takes a string from user and print the index number of third occurance of 'o' in that string. Assume that the string will have atleast three 'o'.
+# example:
+"""
+input: royal technosoft
+output: 13
+"""
