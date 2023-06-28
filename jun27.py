597221581a07d50502ff01c3934fb9f55657420c
alakhpandya <alakhpandya@gmail.com>
2023-06-28 20:55:34 +0530

    Create jun27.py

---
+++ jun27.py
@@ -0,0 +1,56 @@
+s1 = "write a program & take names & marks of 4 subjects from user and create a proper table of sub and marks"
+
+# print(s1.isupper())
+# print(s1.islower())
+
+s2 = "Most\tImportant\tSession¡"
+# print(s2.istitle())
+# print(s2.isnumeric())
+
+s3 = "27-06-2023"
+# print(s3.isnumeric())
+# print(s2.isalpha())
+
+s4 = "MostImportantSession"
+# print(s4.isalpha())
+
+s5 = "2023"
+
+s6 = "MostImportantSession2023"
+# print(s6.isalpha())
+# print(s6.isnumeric())
+# print(s6.isalnum())
+
+# print(s5.isalnum())
+# print(s4.isalnum())
+
+# print(s1.isascii())
+# print(s2.isascii())
+
+# print(s2.isprintable())
+# print(s2)
+
+# print(s1.split())
+# print(s2.split())
+
+contact = 'Yash,Dodiya,987654321,12-12-2003,079-23456789,CTM'
+c1 = contact.split(",")
+# print(c1)
+name = c1[0]
+surname = c1[1]
+# mobile = c1[2]
+dob = c1[3]
+# home = c1[4]
+# address = c1[5]
+# print(name)
+# print(c1[3])
+
+# print(contact.split(",", 2))
+
+print(s1.split('e'))
+
+a = [1,2,3,4,5]
+print(a)
+
+
+# rsplit, partition, rpartition, join method, exercise related to these topics
