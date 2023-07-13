9e8e5a5da6e316be856362f19337748f710fdd79
alakhpandya <alakhpandya@gmail.com>
2023-07-13 18:10:23 +0530

    jul13

---
+++ C Doubts/jul11.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+// print "Hello" as many times as user tells
+int main(){
+    int n, i = 0;
+    printf("How many times?\n");
+    scanf("%d", &n);
+    /*
+    while (i < n){
+        printf("Hello\n");
+        i++;
+    }
+    */
+
+    for(i = 0; i < n; i++){
+        printf("%d. Hello\n", i+1);
+    }
+    return 0;
+}
+// Advance concepts of loop: different values of i
